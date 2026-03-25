0b2c79e00b8cbf69d646e6eb2f357e41d4280e97
Diana Yevstignyeyeva <diana.yevstignyeyeva@gmail.com>
2026-03-25 15:18:09 +0200

    Add sorting and filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
+
 import 'bulma/css/bulma.css';
 import './App.scss';
 
@@ -15,36 +17,100 @@ export const goodsFromServer = [
   'Garlic',
 ];
 
+enum SortType {
+  byAlphabet = 'alphabet',
+  byLength = 'length',
+}
+
+enum FilterType {
+  reverse = 'reverse',
+}
+
 export const App: React.FC = () => {
+  const [filterBy, setFilterBy] = useState<FilterType[]>([]);
+  const [sortBy, setSortBy] = useState<SortType | ''>('');
+
+  const preparedGoods = [...goodsFromServer];
+
+  if (sortBy === SortType.byAlphabet) {
+    preparedGoods.sort((a, b) => {
+      return a.localeCompare(b);
+    });
+  }
+
+  if (sortBy === SortType.byLength) {
+    preparedGoods.sort((a, b) => {
+      return a.length - b.length;
+    });
+  }
+
+  if (filterBy.includes(FilterType.reverse)) {
+    preparedGoods.reverse();
+  }
+
+  function setFilter(filter: FilterType) {
+    setFilterBy(prevFilters => {
+      if (filterBy.includes(filter)) {
+        return prevFilters.filter(i => i !== filter);
+      } else {
+        return [...prevFilters, filter];
+      }
+    });
+  }
+
   return (
     <div className="section content">
       <div className="buttons">
-        <button type="button" className="button is-info is-light">
+        <button
+          type="button"
+          className={cn('button', 'is-info', {
+            'is-light': sortBy !== SortType.byAlphabet,
+          })}
+          onClick={() => setSortBy(SortType.byAlphabet)}
+        >
           Sort alphabetically
         </button>
 
-        <button type="button" className="button is-success is-light">
+        <button
+          type="button"
+          className={cn('button', 'is-success', {
+            'is-light': sortBy !== SortType.byLength,
+          })}
+          onClick={() => setSortBy(SortType.byLength)}
+        >
           Sort by length
         </button>
 
-        <button type="button" className="button is-warning is-light">
+        <button
+          type="button"
+          className={cn('button', 'is-warning', {
+            'is-light': !filterBy.includes(FilterType.reverse),
+          })}
+          onClick={() => setFilter(FilterType.reverse)}
+        >
           Reverse
         </button>
 
-        <button type="button" className="button is-danger is-light">
-          Reset
-        </button>
+        {(filterBy.length !== 0 || sortBy !== '') && (
+          <button
+            type="button"
+            className="button is-danger is-light"
+            onClick={() => {
+              setFilterBy([]);
+              setSortBy('');
+            }}
+          >
+            Reset
+          </button>
+        )}
       </div>
 
       <ul>
-        <ul>
-          <li data-cy="Good">Dumplings</li>
-          <li data-cy="Good">Carrot</li>
-          <li data-cy="Good">Eggs</li>
-          <li data-cy="Good">Ice cream</li>
-          <li data-cy="Good">Apple</li>
-          <li data-cy="Good">...</li>
-        </ul>
+        {preparedGoods.map(good => (
+          <li data-cy="Good" key={good}>
+            {good}
+          </li>
+        ))}
       </ul>
     </div>
   );
